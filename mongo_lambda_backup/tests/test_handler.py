eab0ebe769a3c716041d90386f031c6b9b3a424d
Fresh Talent <freshtalent@fresh.com>
2022-03-30 03:25:26 +0200

    initial commit

---
+++ mongo_lambda_backup/tests/test_handler.py
@@ -0,0 +1,114 @@
+import unittest
+from unittest.mock import patch
+from os import environ
+from json import loads
+
+import boto3 as boto
+from botocore.exceptions import ClientError
+from moto import mock_s3
+from mongomock import MongoClient as MockMongoClient
+
+from mongo_lambda_backup.handler import handler
+
+
+@patch.dict(
+    environ,
+    {
+        "BUCKET_NAME": "mongo-lambda-backup-1",
+        "MONGO_URI": "mongodb://localhost/test-db",
+        "COLLECTION_BLACKLIST": "skip,skip2",
+    },
+)
+@mock_s3
+class TestHandler(unittest.TestCase):
+    def setUp(self):
+        self.conn = boto.resource("s3", region_name="eu-central-1")
+        self.conn.create_bucket(Bucket="mongo-lambda-backup-1")
+
+        self.client = MockMongoClient("mongodb://localhost")
+        database = self.client.get_database("test-db")
+        database.create_collection("unittest")
+        database["unittest"].insert_one({"this": "is", "data": ["some"]})
+        database["unittest"].insert_one({"this": "is", "data": ["other"]})
+
+        database.create_collection("skip")
+        database["skip"].insert_one({"key": 1})
+        database["skip"].insert_one({"key": 2})
+
+    @patch("mongo_lambda_backup.handler.MongoClient")
+    def test_handler(self, mock_constructor):
+        mock_constructor.return_value = self.client
+
+        handler(None, None)
+
+        body = (
+            self.conn.Object("mongo-lambda-backup-1", "backups/unittest.json")
+            .get()["Body"]
+            .read()
+            .decode("utf-8")
+        )
+        for line in body.split("\n"):
+            if line:
+                doc = loads(line)
+                assert doc.get("_id")
+                assert doc.get("this")
+                assert doc.get("data")
+
+        try:
+            self.conn.Object("mongo-lambda-backup-1", "backups/skip.json").get()
+        except ClientError:
+            pass
+        else:
+            assert False
+
+
+@patch.dict(
+    environ,
+    {
+        "BUCKET_NAME": "mongo-lambda-backup-2",
+        "MONGO_URI": "mongodb://localhost/test-db",
+        "COLLECTION_BLACKLIST": "skip,skip2",
+        "IN_MEMORY": "True",
+    },
+)
+@mock_s3
+class TestHandlerInMemory(unittest.TestCase):
+    def setUp(self):
+        self.conn = boto.resource("s3", region_name="eu-central-1")
+        self.conn.create_bucket(Bucket="mongo-lambda-backup-2")
+
+        self.client = MockMongoClient("mongodb://localhost")
+        database = self.client.get_database("test-db")
+        database.create_collection("unittest")
+        database["unittest"].insert_one({"this": "is", "data": ["some"]})
+        database["unittest"].insert_one({"this": "is", "data": ["other"]})
+
+        database.create_collection("skip")
+        database["skip"].insert_one({"key": 1})
+        database["skip"].insert_one({"key": 2})
+
+    @patch("mongo_lambda_backup.handler.MongoClient")
+    def test_handler(self, mock_constructor):
+        mock_constructor.return_value = self.client
+
+        handler(None, None)
+
+        body = (
+            self.conn.Object("mongo-lambda-backup-2", "backups/unittest.json")
+            .get()["Body"]
+            .read()
+            .decode("utf-8")
+        )
+        for line in body.split("\n"):
+            if line:
+                doc = loads(line)
+                assert doc.get("_id")
+                assert doc.get("this")
+                assert doc.get("data")
+
+        try:
+            self.conn.Object("mongo-lambda-backup-2", "backups/skip.json").get()
+        except ClientError:
+            pass
+        else:
+            assert False
